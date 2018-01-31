44d88a7b474a28073ca6ebd009c4bb35cb1bd3ec
Tomasz Setkowski <tom@ai-traders.com>
2018-01-31 16:15:11 +0000

    added ait_mounts to handle nova on gluster and other types #12372

---
+++ ansible/roles/nova/templates/ait_mounts.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# This script is used by nova_compute and nova_libvirt containers to setup mount points for nova guests.
+# Script is mounted with :ro into both containers.
+
+set -e
+
+function check_mounted {
+  dir=$1
+  if ! mountpoint -q -- "$dir" ; then
+     echo "$dir is not mounted. Most recent logs:"
+     tail /var/log/glusterfs/*.log
+     exit 2
+  fi
+}
+
+echo "nova-compute type=$AIT_NOVA_COMPUTE_TYPE"
+if [ -z "$AIT_NOVA_COMPUTE_TYPE" ];
+  then echo "AIT_NOVA_COMPUTE_TYPE is not set";
+  exit 5
+fi
+if [ $AIT_NOVA_COMPUTE_TYPE == "p" ]; then
+    if [ -z "$AIT_NOVA_INSTANCES_MOUNT_OPTS" ];
+      then echo "AIT_NOVA_INSTANCES_MOUNT_OPTS is not set";
+      exit 5
+    elif [ -z "$AIT_NOVA_INSTANCES_BASE_MOUNT_OPTS" ];
+      then echo "AIT_NOVA_INSTANCES_BASE_MOUNT_OPTS is not set";
+      exit 5
+    else
+      mkdir -p /var/lib/nova/instances
+      mount -t glusterfs $AIT_NOVA_INSTANCES_MOUNT_OPTS /var/lib/nova/instances
+      mkdir -p /var/lib/nova/instances/_base
+      mount -t glusterfs $AIT_NOVA_INSTANCES_BASE_MOUNT_OPTS /var/lib/nova/instances/_base
+      chown nova:nova /var/lib/nova/instances /var/lib/nova/instances/_base
+
+      check_mounted "/var/lib/nova/instances"
+      check_mounted "/var/lib/nova/instances/_base"
+    fi
+else
+  echo "AIT_NOVA_COMPUTE_TYPE must be set to p,w or v. Got $AIT_NOVA_COMPUTE_TYPE"
+  exit 4
+fi
