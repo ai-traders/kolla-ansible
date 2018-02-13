dcb212d09509b543fa398865b96ae1980e93a632
Tomasz Setkowski <tom@ai-traders.com>
2018-02-13 12:53:53 +0000

    ceph mixed osd bootstrap script #12464
    Change-Id: I7f7abc01f57bf99933c51c752f07b8181217fe43

--- ansible/roles/ceph/files/osd_extend_start.sh
+++ ansible/roles/ceph/files/osd_extend_start.sh
@@ -7,14 +7,134 @@ if [[ $(stat -c %a /var/log/kolla/ceph) != "755" ]]; then
     chmod 755 /var/log/kolla/ceph
 fi
 
-# Bootstrap and exit if KOLLA_BOOTSTRAP variable is set. This catches all cases
-# of the KOLLA_BOOTSTRAP variable being set, including empty.
-if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
-    set -x
-    # NOTE(SamYaple): Static gpt partcodes
-    CEPH_JOURNAL_TYPE_CODE="45B0969E-9B03-4F30-B4C6-B4B80CEFF106"
-    CEPH_OSD_TYPE_CODE="4FBD7E29-9D25-41B8-AFD0-062C0CEFF05D"
+set -x
+# NOTE(SamYaple): Static gpt partcodes
+CEPH_JOURNAL_TYPE_CODE="45B0969E-9B03-4F30-B4C6-B4B80CEFF106"
+CEPH_OSD_TYPE_CODE="4FBD7E29-9D25-41B8-AFD0-062C0CEFF05D"
 
+if [[ "${!KOLLA_MIX_BOOTSTRAP[@]}" ]]; then
+  # Wait for ceph quorum before proceeding
+  ceph quorum_status
+  if [[ "${OSD_FILESYSTEM}" != "bluestore" ]]; then
+    echo "OSD filesystem must be bluestore"
+    exit 5
+  fi
+
+  OSD_DEV_HDD=${OSD_DEV}
+  OSD_DEV_SSD=${BLUEDB_DEV}
+  sgdisk --zap-all -- "${OSD_DEV_HDD}"
+  sgdisk --zap-all -- "${OSD_DEV_SSD}"
+
+  # Partitions for HDD with SSD elements:
+  sgdisk --new=4:1M:+100M -- "${OSD_DEV_HDD}"
+  # Journal
+  sgdisk --new=2:0M:+512M -- "${OSD_DEV_SSD}"
+
+  # Bluestore RocksDB on SSD, with 1% size of HDD
+  TOTAL_SIZE=$(parted --script $OSD_DEV_HDD unit GB print | awk 'match($0, /^Disk.* (.*)GB/, a){printf("%.2f", a[1])}')
+  KV_SIZE=$(echo "$TOTAL_SIZE * 0.01" | bc -l)
+  KV_SIZE=$(LC_ALL=C /usr/bin/printf "%.*f\n" 0 $KV_SIZE)
+  if [[ "$KV_SIZE" == "0" ]]; then
+    KV_SIZE="1"
+  fi
+  sgdisk --new=3:0:+${KV_SIZE}G -- "${OSD_DEV_SSD}"
+
+  # Use the rest of HDD drive for main OSD data:
+  sgdisk --largest-new=1 -- "${OSD_DEV_HDD}"
+
+  # Partition SSD for second OSD:
+  sgdisk --new=4:0:+100M -- "${OSD_DEV_SSD}"
+  sgdisk --largest-new=1 -- "${OSD_DEV_SSD}"
+
+  # format the metadata filesystems.
+  OSD_PARTITION_HDD="${OSD_DEV_HDD}4"
+  OSD_PARTITION_SSD="${OSD_DEV_SSD}4"
+  mkfs -t xfs -f -i size=2048 -- ${OSD_PARTITION_HDD}
+  mkfs -t xfs -f -i size=2048 -- ${OSD_PARTITION_SSD}
+
+  # Now prepare HDD OSD:
+  OSD_ID_HDD=$(ceph osd create)
+  OSD_DIR_HDD="/var/lib/ceph/osd/ceph-${OSD_ID_HDD}"
+  mkdir -p "${OSD_DIR_HDD}"
+  mount "${OSD_PARTITION_HDD}" "${OSD_DIR_HDD}"
+
+  # Prepare OSD metadata, including symlinks to external devices for journal and RocksDB:
+  echo "bluestore" > "${OSD_DIR_HDD}/type"
+  cd ${OSD_DIR_HDD}
+  BLOCK_PARTITION_HDD="${OSD_DEV_HDD}1"
+  ln -s "${BLOCK_PARTITION_HDD}" block
+  JOURNAL_PARTITION="${OSD_DEV_SSD}2"
+  ln -s "${JOURNAL_PARTITION}" block.wal
+  KV_DB_PARTITION="${OSD_DEV_SSD}3"
+  ln -s "${KV_DB_PARTITION}" block.db
+  ceph-osd -i "${OSD_ID_HDD}" --mkfs --mkkey
+
+  OSD_INITIAL_WEIGHT=$(parted --script ${BLOCK_PARTITION_HDD} unit TB print | awk 'match($0, /^Disk.* (.*)TB/, a){printf("%.2f", a[1])}')
+  ceph auth add "osd.${OSD_ID_HDD}" osd 'allow *' mon 'allow profile osd' -i "${OSD_DIR_HDD}/keyring"
+  cd /
+  umount "${OSD_PARTITION_HDD}"
+  ceph osd crush set-device-class hdd "osd.${OSD_ID_HDD}"
+
+  # Set disk labels so that kolla can pick it up:
+  OSD_PARTITION_NUM=1
+  JOURNAL_PARTITION_NUM=2
+  KV_PARTITION_NUM=3
+  META_PARTITION_NUM=4
+  sgdisk "--change-name=${META_PARTITION_NUM}:KOLLA_CEPH_DATA_${OSD_ID_HDD}" "--typecode=${META_PARTITION_NUM}:${CEPH_OSD_TYPE_CODE}" -- "${OSD_DEV_HDD}"
+  sgdisk "--change-name=${OSD_PARTITION_NUM}:KOLLA_CEPH_DATA_${OSD_ID_HDD}_BLUESTORE" "--typecode=${OSD_PARTITION_NUM}:${CEPH_OSD_TYPE_CODE}" -- "${OSD_DEV_HDD}"
+  sgdisk "--change-name=${JOURNAL_PARTITION_NUM}:KOLLA_CEPH_DATA_${OSD_ID_HDD}_J" "--typecode=${JOURNAL_PARTITION_NUM}:${CEPH_JOURNAL_TYPE_CODE}" -- "${OSD_DEV_SSD}"
+  sgdisk "--change-name=${KV_PARTITION_NUM}:KOLLA_CEPH_DATA_${OSD_ID_HDD}_BLUEDB" "--typecode=${KV_PARTITION_NUM}:${CEPH_OSD_TYPE_CODE}" -- "${OSD_DEV_SSD}"
+
+  # Then to secure devices not messing up their names, we use above labels in the symlinks:
+  mount "${OSD_PARTITION_HDD}" "${OSD_DIR_HDD}"
+  cd ${OSD_DIR_HDD}
+  BLOCK_PARTITION="/dev/disk/by-partlabel/KOLLA_CEPH_DATA_${OSD_ID_HDD}_BLUESTORE"
+  ln -sf "${BLOCK_PARTITION}" block
+  JOURNAL_PARTITION="/dev/disk/by-partlabel/KOLLA_CEPH_DATA_${OSD_ID_HDD}_J"
+  ln -sf "${JOURNAL_PARTITION}" block.wal
+  KV_DB_PARTITION="/dev/disk/by-partlabel/KOLLA_CEPH_DATA_${OSD_ID_HDD}_BLUEDB"
+  ln -sf "${KV_DB_PARTITION}" block.db
+
+  cd /
+  umount "${OSD_PARTITION_HDD}"
+  echo "HDD OSD prepared."
+
+  OSD_ID_SSD=$(ceph osd create)
+  OSD_DIR_SSD="/var/lib/ceph/osd/ceph-${OSD_ID_SSD}"
+  mkdir -p "${OSD_DIR_SSD}"
+  mount "${OSD_PARTITION_SSD}" "${OSD_DIR_SSD}"
+
+  # Prepare OSD metadata, including symlink to block device
+  echo "bluestore" > "${OSD_DIR_SSD}/type"
+  cd ${OSD_DIR_SSD}
+  BLOCK_PARTITION_SSD="${OSD_DEV_SSD}1"
+  ln -s "${BLOCK_PARTITION_SSD}" block
+  ceph-osd -i "${OSD_ID_SSD}" --mkfs --mkkey
+
+  OSD_INITIAL_WEIGHT=$(parted --script ${BLOCK_PARTITION_SSD} unit TB print | awk 'match($0, /^Disk.* (.*)TB/, a){printf("%.2f", a[1])}')
+  ceph auth add "osd.${OSD_ID_SSD}" osd 'allow *' mon 'allow profile osd' -i "${OSD_DIR_SSD}/keyring"
+  cd /
+  umount "${OSD_PARTITION_SSD}"
+  ceph osd crush set-device-class ssd "osd.${OSD_ID_SSD}"
+
+  # Set disk labels so that kolla can pick it up:
+  OSD_PARTITION_NUM=1
+  META_PARTITION_NUM=4
+  sgdisk "--change-name=${META_PARTITION_NUM}:KOLLA_CEPH_DATA_${OSD_ID_SSD}" "--typecode=${META_PARTITION_NUM}:${CEPH_OSD_TYPE_CODE}" -- "${OSD_DEV_SSD}"
+  sgdisk "--change-name=${OSD_PARTITION_NUM}:KOLLA_CEPH_DATA_${OSD_ID_SSD}_BLUESTORE" "--typecode=${OSD_PARTITION_NUM}:${CEPH_OSD_TYPE_CODE}" -- "${OSD_DEV_SSD}"
+
+  # secure devices not messing up their names, we use above labels in the symlinks:
+  mount "${OSD_PARTITION_SSD}" "${OSD_DIR_SSD}"
+  cd ${OSD_DIR_SSD}
+  BLOCK_PARTITION="/dev/disk/by-partlabel/KOLLA_CEPH_DATA_${OSD_ID_SSD}_BLUESTORE"
+  ln -sf "${BLOCK_PARTITION}" block
+
+  cd /
+  umount "${OSD_PARTITION_SSD}"
+
+  exit 0
+
+elif [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     # Wait for ceph quorum before proceeding
     ceph quorum_status
 
@@ -24,22 +144,6 @@ if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
       sgdisk --zap-all -- "${OSD_DEV}"
       # A small data partition, where configuration files, keys and UUIDs reside.
       sgdisk --new=4:1M:+100M -- "${OSD_DEV}"
-      # # WAL, journal
-      # if [[ "${USE_EXTERNAL_JOURNAL}" == "True" ]]; then
-      #   echo "Using external device for journal ${JOURNAL_DEV}"
-      #   sgdisk --new=2:0M:+1024M -- "${JOURNAL_DEV}"
-      # fi
-      # if [[ "${USE_EXTERNAL_BLUEDB}" == "True" ]]; then
-      #   # RocksDB, By default a partition will be created on the device that is 1% of the main device size.
-      #   TOTAL_SIZE=$(parted --script $OSD_DEV unit GB print | awk 'match($0, /^Disk.* (.*)GB/, a){printf("%.2f", a[1])}')
-      #   # compute 1% and round up
-      #   KV_SIZE=$(echo "$TOTAL_SIZE * 0.01" | bc -l)
-      #   KV_SIZE=$(LC_ALL=C /usr/bin/printf "%.*f\n" 0 $KV_SIZE)
-      #   if [[ "$KV_SIZE" == "0" ]]; then
-      #     KV_SIZE="1"
-      #   fi
-      #   sgdisk --new=3:0:+${KV_SIZE}G -- "${BLUE_DEV}"
-      # fi
       # The actual data on the rest of main device
       sgdisk --largest-new=1 -- "${OSD_DEV}"
       partprobe || true
@@ -47,9 +151,6 @@ if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
       OSD_PARTITION="${OSD_DEV}4"
       mkfs -t xfs -f -i size=2048 -- ${OSD_PARTITION}
 
-      # OSD_DATA_PARTITION_NUM=1
-      # JOURNAL_PARTITION_NUM=2
-      # KV_PARTITION_NUM=3
       META_PARTITION_NUM=4
     else
       if [[ "${USE_EXTERNAL_JOURNAL}" == "False" ]]; then
