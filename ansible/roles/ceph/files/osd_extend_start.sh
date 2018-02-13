98ef44bd47fe4f8f43d9cb90753a59cc6be21554
Tomasz Setkowski <tom@ai-traders.com>
2018-02-13 09:27:13 +0000

    handle bluestore without partitioning drive #12463
    - This drops most of bootstrapping support, because we don't need it and
    will not do it like that anyway
    - Instead start OSD containers is better now because it supports
    discovery of OSD devices. So we can have OSDs with just one backing
    drives or a mix of 3. The bootstrapping can be done manually/by other
    process.
    
    Change-Id: Ib5af05c67375d89531abc932bed4cc06250c5d64

--- ansible/roles/ceph/files/osd_extend_start.sh
+++ ansible/roles/ceph/files/osd_extend_start.sh
@@ -24,17 +24,22 @@ if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
       sgdisk --zap-all -- "${OSD_DEV}"
       # A small data partition, where configuration files, keys and UUIDs reside.
       sgdisk --new=4:1M:+100M -- "${OSD_DEV}"
-      # WAL, journal
-      sgdisk --new=2:0M:+1024M -- "${JOURNAL_DEV}"
-      # RocksDB, By default a partition will be created on the device that is 1% of the main device size.
-      TOTAL_SIZE=$(parted --script $OSD_DEV unit GB print | awk 'match($0, /^Disk.* (.*)GB/, a){printf("%.2f", a[1])}')
-      # compute 1% and round up
-      KV_SIZE=$(echo "$TOTAL_SIZE * 0.01" | bc -l)
-      KV_SIZE=$(LC_ALL=C /usr/bin/printf "%.*f\n" 0 $KV_SIZE)
-      if [[ "$KV_SIZE" == "0" ]]; then
-        KV_SIZE="1"
-      fi
-      sgdisk --new=3:0:+${KV_SIZE}G -- "${OSD_DEV}"
+      # # WAL, journal
+      # if [[ "${USE_EXTERNAL_JOURNAL}" == "True" ]]; then
+      #   echo "Using external device for journal ${JOURNAL_DEV}"
+      #   sgdisk --new=2:0M:+1024M -- "${JOURNAL_DEV}"
+      # fi
+      # if [[ "${USE_EXTERNAL_BLUEDB}" == "True" ]]; then
+      #   # RocksDB, By default a partition will be created on the device that is 1% of the main device size.
+      #   TOTAL_SIZE=$(parted --script $OSD_DEV unit GB print | awk 'match($0, /^Disk.* (.*)GB/, a){printf("%.2f", a[1])}')
+      #   # compute 1% and round up
+      #   KV_SIZE=$(echo "$TOTAL_SIZE * 0.01" | bc -l)
+      #   KV_SIZE=$(LC_ALL=C /usr/bin/printf "%.*f\n" 0 $KV_SIZE)
+      #   if [[ "$KV_SIZE" == "0" ]]; then
+      #     KV_SIZE="1"
+      #   fi
+      #   sgdisk --new=3:0:+${KV_SIZE}G -- "${BLUE_DEV}"
+      # fi
       # The actual data on the rest of main device
       sgdisk --largest-new=1 -- "${OSD_DEV}"
       partprobe || true
@@ -42,9 +47,9 @@ if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
       OSD_PARTITION="${OSD_DEV}4"
       mkfs -t xfs -f -i size=2048 -- ${OSD_PARTITION}
 
-      OSD_DATA_PARTITION_NUM=1
-      JOURNAL_PARTITION_NUM=2
-      KV_PARTITION_NUM=3
+      # OSD_DATA_PARTITION_NUM=1
+      # JOURNAL_PARTITION_NUM=2
+      # KV_PARTITION_NUM=3
       META_PARTITION_NUM=4
     else
       if [[ "${USE_EXTERNAL_JOURNAL}" == "False" ]]; then
@@ -76,21 +81,29 @@ if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     if [[ "${OSD_FILESYSTEM}" == "bluestore" ]]; then
         echo "bluestore" > "${OSD_DIR}/type"
         cd ${OSD_DIR}
+        # block symlink is the only one required.
         BLOCK_PARTITION="${OSD_DEV}1"
-        JOURNAL_PARTITION="${JOURNAL_DEV}2"
-        KV_DB_DEV="${OSD_DEV}3"
         ln -s "${BLOCK_PARTITION}" block
-        ln -s "${JOURNAL_PARTITION}" block.wal
-        ln -s "${KV_DB_DEV}" block.db
+
+        if [[ "${USE_EXTERNAL_JOURNAL}" == "True" ]]; then
+          #JOURNAL_PARTITION="${JOURNAL_DEV}2"
+          ln -s "${JOURNAL_PARTITION}" block.wal
+        fi
+
+        if [[ "${USE_EXTERNAL_BLUEDB}" == "True" ]]; then
+          #KV_DB_PARTITION="${BLUE_DEV}3"
+          ln -s "${KV_DB_PARTITION}" block.db
+        fi
 
         if [[ "${OSD_INITIAL_WEIGHT}" == "auto" ]]; then
             OSD_INITIAL_WEIGHT=$(parted --script ${BLOCK_PARTITION} unit TB print | awk 'match($0, /^Disk.* (.*)TB/, a){printf("%.2f", a[1])}')
         fi
+
+        ceph-osd -i "${OSD_ID}" --mkfs --mkkey
+    else
+        ceph-osd -i "${OSD_ID}" --mkfs --osd-journal="${JOURNAL_PARTITION}" --mkkey
     fi
 
-    # This will throw an error about no key existing. That is normal. It then
-    # creates the key in the next step.
-    ceph-osd -i "${OSD_ID}" --mkfs --osd-journal="${JOURNAL_PARTITION}" --mkkey
     ceph auth add "osd.${OSD_ID}" osd 'allow *' mon 'allow profile osd' -i "${OSD_DIR}/keyring"
     cd /
     umount "${OSD_PARTITION}"
@@ -104,30 +117,36 @@ if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     fi
 
     # Setting partition name based on ${OSD_ID}
-    sgdisk "--change-name=${JOURNAL_PARTITION_NUM}:KOLLA_CEPH_DATA_${OSD_ID}_J" "--typecode=${JOURNAL_PARTITION_NUM}:${CEPH_JOURNAL_TYPE_CODE}" -- "${JOURNAL_DEV}"
     if [[ "${OSD_FILESYSTEM}" == "bluestore" ]]; then
       # In bluestore case, the actual data is really on ${BLOCK_PARTITION}, that it is not a filesystem.
       # Since this information is only for kolla to use for mounting in container's host, we set 'ceph data'
       # label on the 100M partition which is really meant to be mounted and contains neccesary metadata files
       sgdisk "--change-name=${META_PARTITION_NUM}:KOLLA_CEPH_DATA_${OSD_ID}" "--typecode=${META_PARTITION_NUM}:${CEPH_OSD_TYPE_CODE}" -- "${OSD_DEV}"
       # For sake of completeness, we set labels on KV partion and bluestore too:
-      sgdisk "--change-name=${OSD_PARTITION_NUM}:KOLLA_CEPH_${OSD_ID}_BLUESTORE" "--typecode=${OSD_PARTITION_NUM}:${CEPH_OSD_TYPE_CODE}" -- "${OSD_DEV}"
-      sgdisk "--change-name=${KV_PARTITION_NUM}:KOLLA_CEPH_${OSD_ID}_BLUE_KV" "--typecode=${KV_PARTITION_NUM}:${CEPH_OSD_TYPE_CODE}" -- "${OSD_DEV}"
+      sgdisk "--change-name=${OSD_PARTITION_NUM}:KOLLA_CEPH_DATA_${OSD_ID}_BLUESTORE" "--typecode=${OSD_PARTITION_NUM}:${CEPH_OSD_TYPE_CODE}" -- "${OSD_DEV}"
 
-      if [[ "${OSD_FILESYSTEM}" == "bluestore" ]]; then
-          echo "Setting strict links for bluestore components"
-          mount "${OSD_PARTITION}" "${OSD_DIR}"
-          cd ${OSD_DIR}
-          BLOCK_PARTITION="/dev/disk/by-partlabel/KOLLA_CEPH_${OSD_ID}_BLUESTORE"
-          JOURNAL_PARTITION="/dev/disk/by-partlabel/KOLLA_CEPH_DATA_${OSD_ID}_J"
-          KV_DB_DEV="/dev/disk/by-partlabel/KOLLA_CEPH_${OSD_ID}_BLUE_KV"
-          ln -sf "${BLOCK_PARTITION}" block
-          ln -sf "${JOURNAL_PARTITION}" block.wal
-          ln -sf "${KV_DB_DEV}" block.db
-          cd /
-          umount "${OSD_PARTITION}"
+      echo "Setting strict links for bluestore components"
+      mount "${OSD_PARTITION}" "${OSD_DIR}"
+      cd ${OSD_DIR}
+      BLOCK_PARTITION="/dev/disk/by-partlabel/KOLLA_CEPH_DATA_${OSD_ID}_BLUESTORE"
+      ln -sf "${BLOCK_PARTITION}" block
+
+      if [[ "${USE_EXTERNAL_JOURNAL}" == "True" ]]; then
+        sgdisk "--change-name=${JOURNAL_PARTITION_NUM}:KOLLA_CEPH_DATA_${OSD_ID}_J" "--typecode=${JOURNAL_PARTITION_NUM}:${CEPH_JOURNAL_TYPE_CODE}" -- "${JOURNAL_DEV}"
+        JOURNAL_PARTITION="/dev/disk/by-partlabel/KOLLA_CEPH_DATA_${OSD_ID}_J"
+        ln -sf "${JOURNAL_PARTITION}" block.wal
       fi
+
+      if [[ "${USE_EXTERNAL_BLUEDB}" == "True" ]]; then
+        sgdisk "--change-name=${KV_PARTITION_NUM}:KOLLA_CEPH_DATA_${OSD_ID}_BLUEDB" "--typecode=${KV_PARTITION_NUM}:${CEPH_OSD_TYPE_CODE}" -- "${BLUE_DEV}"
+        KV_DB_PARTITION="/dev/disk/by-partlabel/KOLLA_CEPH_DATA_${OSD_ID}_BLUEDB"
+        ln -sf "${KV_DB_PARTITION}" block.db
+      fi
+
+      cd /
+      umount "${OSD_PARTITION}"
     else
+      sgdisk "--change-name=${JOURNAL_PARTITION_NUM}:KOLLA_CEPH_DATA_${OSD_ID}_J" "--typecode=${JOURNAL_PARTITION_NUM}:${CEPH_JOURNAL_TYPE_CODE}" -- "${JOURNAL_DEV}"
       sgdisk "--change-name=${OSD_PARTITION_NUM}:KOLLA_CEPH_DATA_${OSD_ID}" "--typecode=${OSD_PARTITION_NUM}:${CEPH_OSD_TYPE_CODE}" -- "${OSD_DEV}"
     fi
 
@@ -135,4 +154,9 @@ if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
 fi
 
 OSD_DIR="/var/lib/ceph/osd/ceph-${OSD_ID}"
-ARGS="-i ${OSD_ID} --osd-journal ${JOURNAL_PARTITION} -k ${OSD_DIR}/keyring"
+if [[ "${OSD_FILESYSTEM}" == "bluestore" ]]; then
+  # do not use osd-journal param because it sets
+  ARGS="-i ${OSD_ID} -k ${OSD_DIR}/keyring"
+else
+  ARGS="-i ${OSD_ID} --osd-journal ${JOURNAL_PARTITION} -k ${OSD_DIR}/keyring"
+fi
